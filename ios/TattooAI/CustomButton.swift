06e5efa6ddebcdf7294774c169d97468d47f692c
Nick <n9163293481@gmail.com>
2021-02-07 17:33:02 +0300

    Started GeneratorView

---
+++ ios/TattooAI/CustomButton.swift
@@ -0,0 +1,22 @@
+//
+//  CustomButton.swift
+//  TattooAI
+//
+//  Created by Nikolay Kryuchkov on 07.02.2021.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+
+}
