4a1324b308998bb305014289ea69b72eeef57b70
Nick <n9163293481@gmail.com>
2021-02-11 08:38:11 +0300

    Tunned up Generator UI

--- ios/TattooAI/GeneratorViewController.swift
+++ ios/TattooAI/GeneratorViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import Kingfisher
 
 class GeneratorViewController: UIViewController {
 
@@ -13,10 +15,25 @@ class GeneratorViewController: UIViewController {
     @IBOutlet weak var placeLabel: UILabel!
     @IBOutlet weak var styleLabel: UILabel!
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var generateButton: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageView.layer.cornerRadius = 10
+//        likeButton.setImage(UIImage(named: "like")?.withRenderingMode(.alwaysTemplate),
+//                            for: .normal)
+//        likeButton.tintColor = .white
+//        likeButton.tintColor = UIColor(red: MainColor.red,
+//                                       green: MainColor.green,
+//                                       blue: MainColor.blue,
+//                                       alpha: 1.0)
+        settingsButton.layer.cornerRadius = 10
+        generateButton.layer.masksToBounds = true
+        generateButton.layer.cornerRadius = 10
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
