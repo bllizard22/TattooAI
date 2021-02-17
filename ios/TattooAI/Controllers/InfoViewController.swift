7ec7a2c96f710593ef2d3e0582a95ada70739687
Nick <n9163293481@gmail.com>
2021-02-17 17:45:38 +0300

    Still working on contact links in InfoVC

--- ios/TattooAI/Controllers/InfoViewController.swift
+++ ios/TattooAI/Controllers/InfoViewController.swift
@@ -12,15 +12,19 @@ class InfoViewController: UIViewController {
     @IBOutlet weak var hiwButton: UIButton!
     @IBOutlet weak var melInstagram: UILabel!
     
+    let contacts = ["@evgenymel": "https://www.instagram.com/evgenymel/",
+                    "@name": "name@mail.com",
+                    "@bllizard22": "https://t.me/bllizard22"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         overrideUserInterfaceStyle = .dark
         hiwButton.layer.cornerRadius = 10
         
-        let tapMelIG = UITapGestureRecognizer(target: self, action: #selector(InfoViewController.melIGDidPressed(_:)))
+        let tapMelInst = UITapGestureRecognizer(target: self, action: #selector(InfoViewController.melIGDidPressed(_:)))
         melInstagram.isUserInteractionEnabled = true
-        melInstagram.addGestureRecognizer(tapMelIG)
+        melInstagram.addGestureRecognizer(tapMelInst)
         let attributedString = NSMutableAttributedString(string: "@evgenymel")
 ////        let attributedString = NSMutableAttributedString(string: "mel", attributes:[NSAttributedString.Key.link: URL(string: "http://www.google.com/")!])
         attributedString.addAttribute(.underlineStyle,
@@ -35,7 +39,11 @@ class InfoViewController: UIViewController {
     
     @IBAction func melIGDidPressed(_ sender: UITapGestureRecognizer) {
         print("tap working")
-        UIApplication.shared.open(URL(string: "https://www.instagram.com/evgenymel/")!,
+//        UIApplication.shared.open(URL(string: "https://www.instagram.com/evgenymel/")!,
+//                                  options: [:],
+//                                  completionHandler: nil)
+        print(sender.view?.tag)
+        UIApplication.shared.open(URL(string: contacts["@evgenymel"]!)!,
                                   options: [:],
                                   completionHandler: nil)
     }
